0591b595f537b5d4ef7c1783bab661dcbf9f45b1
NotThanaphat <thanapat.pingkaew@gmail.com>
2026-02-13 12:24:30 +0700

    upload lab18 1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,9 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1, Rect r2){
+    double w_overlap = min(r1.x + r1.w, r2.x + r2.w) - max(r1.x, r2.x);
+    double h_overlap = min(r1.y, r2.y) - max(r1.y - r1.h, r2.y - r2.h);
+    if(w_overlap <= 0 || h_overlap <= 0) return 0;
+    return w_overlap * h_overlap;
 }
